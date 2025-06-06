8e0d990d5df9cf7893bba2964e43d6408744035c
sowmathi761 <sowmathi37@gmail.com>
2025-06-06 14:55:08 +0530

    first command

---
+++ script(function).js
@@ -0,0 +1,15 @@
+console.log(5=='5')
+console.log(5==='5')
+function sum(c,d){
+    return  c+d
+}
+console.log(sum(30,40));
+ 
+function add(a,b){
+    return  `sum ${a} and ${b} is ${a+b}`
+}
+console.log(add(30,40));
+ 
+
+
+
